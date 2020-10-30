594eb198434ee5bec09dafb364cbeef4d8660c97
A M Akankshit <akankshit.am@ET-C02YQFZGLVCF.local>
2020-10-30 18:03:21 +0530

    State1: Confetti setup is done, Confetti will start on mount and end on unmount. 'requestAnimationFrame' is cancelled on unmount An animation timout is also added though animation stopping is not smooth

--- src/App.js
+++ src/App.js
@@ -1,7 +1,28 @@
-import React from 'react';
+import React, { useState } from 'react';
+import Confetti from './confetti';
 
 function App() {
-  return <div className="App">Hello World</div>;
+  const [showConfetti, setShowConfetti] = useState(false);
+  return (
+    <div className="App">
+      <button
+        onClick={() => setShowConfetti(!showConfetti)}
+        style={{
+          paddin: '30px',
+          background: 'blue',
+          color: 'white',
+          margin: '25px'
+        }}
+      >
+        Click Me to mount/unmount confetti component
+      </button>
+      {showConfetti && (
+        <div style={{ width: '100vh', height: '100vh', background: 'pink' }}>
+          <Confetti animationTimeout={5000} />
+        </div>
+      )}
+    </div>
+  );
 }
 
 export default App;
