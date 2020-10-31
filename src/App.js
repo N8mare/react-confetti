8f7607bfad8372e0508c4696f8fe35c5fd555c39
A M Akankshit <akankshit.am@ET-C02YQFZGLVCF.local>
2020-10-31 10:19:56 +0530

    Stage 6: Multiple component mount will have their separate state and behave as expected. Added options gravity and windspeed. On component unmount the code will break and animation won't be cancelled as the state wont be preserved until that.

--- src/App.js
+++ src/App.js
@@ -4,10 +4,10 @@ import Confetti from './confetti';
 function App() {
   const [showConfetti, setShowConfetti] = useState(false);
   const [streamAnimation, toggleStreamAnimation] = useState(false);
+  const [streamAnimation2, toggleStreamAnimation2] = useState(false);
+
   const [dummyState, setDummyState] = useState(false);
-  const rerender = () => {
-    setDummyState(!dummyState);
-  };
+  const [dummyState2, setDummyState2] = useState(false);
 
   return (
     <div className="App">
@@ -23,11 +23,27 @@ function App() {
         Click Me to mount/unmount confetti component
       </button>
       {showConfetti && (
-        <div style={{ width: '100vh', height: '100vh', background: 'pink' }}>
-          <button onClick={() => toggleStreamAnimation(true)}>start</button>
-          <button onClick={() => toggleStreamAnimation(false)}>stop</button>
-          <button onClick={rerender}>rerender</button>
-          <Confetti options={{ count: 50 }} streamAnimation={streamAnimation} />
+        <div style={{ display: 'flex', 'justify-content': 'space-between' }}>
+          <div style={{ width: '100vh', height: '80vh', background: 'pink' }}>
+            <button onClick={() => toggleStreamAnimation(true)}>start</button>
+            <button onClick={() => toggleStreamAnimation(false)}>stop</button>
+            <button onClick={() => setDummyState(!dummyState)}>rerender</button>
+            <Confetti
+              options={{ count: 50 }}
+              streamAnimation={streamAnimation}
+            />
+          </div>
+          <div style={{ width: '100vh', height: '80vh', background: 'pink' }}>
+            <button onClick={() => toggleStreamAnimation2(true)}>start</button>
+            <button onClick={() => toggleStreamAnimation2(false)}>stop</button>
+            <button onClick={() => setDummyState2(!dummyState2)}>
+              rerender
+            </button>
+            <Confetti
+              options={{ count: 50, gravity: 20 }}
+              streamAnimation={streamAnimation2}
+            />
+          </div>
         </div>
       )}
     </div>
